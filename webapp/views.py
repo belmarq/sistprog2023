1a94ad0f9d80b0a67401fa0d8e83bcd5272f1a50
Luis Beltran <belmarq@hotmail.com>
2023-11-20 21:56:19 -0700

    v2

--- webapp/views.py
+++ webapp/views.py
@@ -8,5 +8,8 @@ class Home(View):
     def get(self, request):
         cdx = {
             'titulo':'Enlaces',
+            'URL':request.build_absolute_uri(),
+            'IP':request.META.get('REMOTE_ADDR'),
+            'puerto':request.META['SERVER_PORT'],
         }
         return render(request, 'index.html', cdx)
